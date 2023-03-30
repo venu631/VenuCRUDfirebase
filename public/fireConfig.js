730eceb19babe8d48793273ebcade345dbca8b43
venu631 <84405258+venu631@users.noreply.github.com>
2023-03-30 21:19:53 +0530

    Add files via upload

---
+++ public/fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD4HUHRpy4o2QTZ_0UyeoGRoOUAtGLYkCk",
+    authDomain: "crud-b4d29.firebaseapp.com",
+    databaseURL: "https://crud-b4d29-default-rtdb.firebaseio.com",
+    projectId: "crud-b4d29",
+    storageBucket: "crud-b4d29.appspot.com",
+    messagingSenderId: "346740730807",
+    appId: "1:346740730807:web:a2e0ecf4ee3e5e0816d632",
+    measurementId: "G-SRSVZG0FC5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
